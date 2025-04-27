f6f68131bcf33d55cebae4abc719d9e71c7b2cb9
celestroccayoli <celestetroccayoli@gmail.com>
2025-04-27 19:08:06 -0300

    fix de tipos de datos

--- src/views/QuestionCard.tsx
+++ src/views/QuestionCard.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 import questionsData from '../data/questions';
 
 interface LocationState {
@@ -8,7 +8,6 @@ interface LocationState {
 
 const QuestionCard: React.FC = () => {
   const { categoryId } = useParams<{ categoryId: string }>();
-  const navigate = useNavigate();
   const location = useLocation();
   const { color } = location.state as LocationState;
 
