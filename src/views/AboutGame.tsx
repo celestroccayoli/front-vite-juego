4257ae739692d5aae272ad016552363b2cf40d8e
celestroccayoli <celestetroccayoli@gmail.com>
2025-05-11 10:41:06 -0300

    fix de startGame

--- src/views/AboutGame.tsx
+++ src/views/AboutGame.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
-import { useNavigate } from "react-router-dom";
 import "./AboutGame.css";
 import AboutCarousel from '../components/AboutCarousel/AboutCarousel';
 
 const AboutGame: React.FC = () => {
-  const navigate = useNavigate();
-
-  const startGame = () => {
-    navigate("/game");
-  };
-
+ 
   return (
     <div className="min-h-screen flex flex-col items-center" style={{ backgroundColor: '#d5d5ed' }}>
       {/* Header */}
